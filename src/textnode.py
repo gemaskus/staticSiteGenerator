ca86381f09deb2be529a1eed4d91ddb931ced974
Grant <gemaskus@icloud.com>
2024-07-29 16:28:51 -0600

    pushing up all the changes from last night, CH 2. 1. TextNode.py

---
+++ src/textnode.py
@@ -0,0 +1,22 @@
+class TextNode():
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+#    def __init__(self, text, text_type):
+#        self.text = text
+#        self.text_type = text_type
+#        self.url = None
+
+
+    def __eq__(self, other):
+        return ((self.text == other.text) 
+                and (self.text_type == other.text_type)
+                and (self.url == other.url))
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def printHello():
+    print("Hello")
